9d29c2ec1b7817a7cd6c298ffc053e4ffa3e2574
winterheartlee <leejonhouse@gmail.com>
2022-03-01 11:03:03 +0000

    events templates, views and urls added

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+
+def all_events(request):
+    """ A view to return all the events that belong to user """
+    events = Event.objects.all()
+
+    context = {
+        'events': events,
+    }
+
+    return render(request, 'events/events.html', context)
