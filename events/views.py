08c3e7885af245e667ae2903c0fe4d2557eb4d3d
winterheartlee <leejonhouse@gmail.com>
2022-03-02 23:34:06 +0000

    search bar configured to search events

--- events/views.py
+++ events/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Event
 
 # Create your views here.
@@ -6,9 +8,21 @@ from .models import Event
 def all_events(request):
     """ A view to return all the events that belong to user """
     events = Event.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('events'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            events = events.filter(queries)
 
     context = {
         'events': events,
+        'search_term': query,
     }
 
     return render(request, 'events/events.html', context)
