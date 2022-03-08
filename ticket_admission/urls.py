2892e486e326069583808ee776b9982793654a63
winterheartlee <leejonhouse@gmail.com>
2022-03-08 11:11:46 +0000

    back to top button added

--- ticket_admission/urls.py
+++ ticket_admission/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('events', include('events.urls')),
+    path('events/', include('events.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
