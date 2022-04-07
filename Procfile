9ed38d89fe1ee40d5b8054f430e7020a8d8e8da6
winterheartlee <leejonhouse@gmail.com>
2022-04-07 16:13:41 +0000

    prepared for heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ticket_admission.wsgi:application
--- ticket_admission/settings.py
+++ ticket_admission/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-(ng*7@+503b2b!yu-9^26t1+76wqx(w*=66_k-oax1u2b&w!9t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ticketspark.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +119,18 @@ WSGI_APPLICATION = 'ticket_admission.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
