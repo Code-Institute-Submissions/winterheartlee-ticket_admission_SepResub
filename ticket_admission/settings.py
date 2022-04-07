d3b20de91ce6e6b49b910bd66022332677425d0d
winterheartlee <leejonhouse@gmail.com>
2022-04-07 16:27:33 +0000

    removed secret key, set debug and configured github to heroku deployment

--- ticket_admission/settings.py
+++ ticket_admission/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ng*7@+503b2b!yu-9^26t1+76wqx(w*=66_k-oax1u2b&w!9t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ticketspark.herokuapp.com', 'localhost']
 
