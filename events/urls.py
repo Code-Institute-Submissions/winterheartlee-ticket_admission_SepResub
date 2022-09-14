ca4f99e021e7b698d3e9a342929da0e0361c0106
winterheartlee <leejonhouse@gmail.com>
2022-09-14 15:31:32 +0000

    Changed events URL pattern

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_events, name='all_events'),
+    path('all_events/', views.all_events, name='all_events'),
     path('my_events/', views.my_events, name='my_events'),
     path('<int:event_id>/', views.event_detail, name='event_detail'),
     path('my_<int:event_id>/', views.my_event_detail, name='my_event_detail'),
