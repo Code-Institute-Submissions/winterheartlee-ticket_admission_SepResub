4fdc508314ae8500699baa576d077c73efcf3ae9
winterheartlee <leejonhouse@gmail.com>
2022-09-14 13:43:31 +0000

    changed event, forms datetime formatting

--- events/forms.py
+++ events/forms.py
@@ -11,8 +11,22 @@ class EventForm(forms.ModelForm):
 
     image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
     # using attrs to send type:datetime-local for HTML5 datetime picker
-    starts = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local'}))
-    ends = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local'}))
+    starts = forms.DateTimeField(
+        input_formats = ['%Y-%m-%dT%H:%M'],
+        widget = forms.DateTimeInput(
+            attrs={
+                'type': 'datetime-local',
+                'class': 'form-control'},
+            format='%Y-%m-%dT%H:%M')
+    )
+    ends = forms.DateTimeField(
+        input_formats = ['%Y-%m-%dT%H:%M'],
+        widget = forms.DateTimeInput(
+            attrs={
+                'type': 'datetime-local',
+                'class': 'form-control'},
+            format='%Y-%m-%dT%H:%M')
+    )
     user = forms.CharField(widget=forms.TextInput(attrs={'id': 'user', 'type': 'hidden'}))
 
     def __init__(self, *args, **kwargs):
